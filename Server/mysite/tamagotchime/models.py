fbfa6fc5cdfea86a61867d601a8a1a743282809f
Brandon White <bwhite06@stac.edu>
2019-01-02 19:21:28 -0500

    working on db
    - working on db tables

--- Server/mysite/tamagotchime/models.py
+++ Server/mysite/tamagotchime/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tamagotchi(models.Model): 
+ id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+ height = models.IntegerField(default=0)
+ weight = models.IntegerField(default=0)
+ age = models.IntegerField(default=0)
+
+ user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+ FEMALE = 'Female'
+ MALE = 'Male'
+    
+ genderChoices = (
+        (FEMALE, 'Female'),
+        (MALE, 'MALE'),
+        
+    )
+ gender = models.CharField(
+        max_length=6,
+        choices=genderChoices,
+        default = FEMALE,
+    )
