4970ee98c9ebca747f5560aa3ef0a47992966063
Keiran <keirankozlowski@gmail.com>
2019-01-02 20:21:26 -0500

    Set up some urls

--- Server/mysite/mysite/urls.py
+++ Server/mysite/mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('tamagotchime.urls')),
+    path('dashboard/', include('tamagotchime.urls')),
+    path('dashboard/journal/', include('tamagotchime.urls')),
+    path('dashboard/settings/', include('tamagotchime.urls')),
     path('admin/', admin.site.urls),
 ]
