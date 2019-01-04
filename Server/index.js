6374a075db6786dbb8e4edcc50231e1774969231
Keiran <keirankozlowski@gmail.com>
2019-01-04 01:05:32 -0500

    moved django stuff into an archive. set up initial node server.

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors(corsOptions));
+
+const port = 8000;
+server.listen(port, () => console.log(`API running on port ${port}`));
+
+// sanity check
+server.get('/', (request, response) => {
+    response.send('Server initialized.');
+});
